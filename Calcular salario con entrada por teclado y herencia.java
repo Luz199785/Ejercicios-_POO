8c679a277cc80a9823ec5196ab3c70d5b5ff5809
Luz199785 <111939240+Luz199785@users.noreply.github.com>
2023-02-20 18:28:15 -0500

    Create Calcular salario con entrada por teclado y herencia.java

---
+++ Calcular salario con entrada por teclado y herencia.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Ingrese el nombre del empleado: ");
+    String nombre = scanner.nextLine();
+
+    System.out.print("Ingrese el salario base del empleado: ");
+    double salarioBase = scanner.nextDouble();
+
+    System.out.print("Ingrese el tipo de empleado (A para asalariado, H para por horas, C para por comision): ");
+    String tipo = scanner.next();
+
+    double salario = 0.0;
+
+    if (tipo.equalsIgnoreCase("A")) {
+      EmpleadoAsalariado empleado = new EmpleadoAsalariado(nombre, salarioBase);
+      salario = empleado.calcularSalario();
+    } else if (tipo.equalsIgnoreCase("H")) {
+      System.out.print("Ingrese las horas trabajadas: ");
+      int horasTrabajadas = scanner.nextInt();
+
+      System.out.print("Ingrese la tarifa por hora: ");
+      double tarifaPorHora = scanner.nextDouble();
+
+      EmpleadoPorHoras empleado = new EmpleadoPorHoras(nombre, salarioBase, horasTrabajadas, tarifaPorHora);
+      salario = empleado.calcularSalario();
+    } else if (tipo.equalsIgnoreCase("C")) {
+      System.out.print("Ingrese el total de ventas: ");
+      double ventas = scanner.nextDouble();
+
+      System.out.print("Ingrese el porcentaje de comisión: ");
+      double porcentajeComision = scanner.nextDouble();
+
+      EmpleadoPorComision empleado = new EmpleadoPorComision(nombre, salarioBase, ventas, porcentajeComision);
+      salario = empleado.calcularSalario();
+    }
+
+    System.out.println("El salario de " + nombre + " es: " + salario);
+  }
+}
+En este ejemplo, se utiliza un objeto Scanner para leer los valores de entrada por teclado. Primero se solicita el nombre del empleado y el salario base. 
+  Luego se pide al usuario que ingrese el tipo de empleado: A para asalariado, H para por horas, C para por comisión.
+
+Dependiendo del tipo de empleado ingresado, se leen los valores adicionales necesarios para calcular el salario. Para empleados por horas, 
+se solicitan las horas trabajadas y la tarifa por hora. Para empleados por comisión, se solicitan el total de ventas y el porcentaje de comisión.
+
+Finalmente, se crea una instancia de la subclase correspondiente y se llama al método calcularSalario() para calcular el salario del empleado.
+  El resultado se imprime en pantalla.
