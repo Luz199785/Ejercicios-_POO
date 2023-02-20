830425614f056ebd7831b206a90cc37dcd736d0d
Luz199785 <111939240+Luz199785@users.noreply.github.com>
2023-02-20 18:25:37 -0500

    Update calcular salario.java

--- calcular salario.java
+++ calcular salario.java
@@ -64,3 +64,12 @@ public class EmpleadoPorComision extends Empleado {
     return super.getSalarioBase() + (ventas * porcentajeComision);
   }
 }
+/*En este ejemplo, la clase Empleado es la superclase, que tiene un atributo nombre y un atributo salarioBase. También tiene un método calcularSalario() que devuelve el salario base del empleado.
+
+La clase EmpleadoAsalariado es una subclase que hereda los atributos y métodos de la clase Empleado. En este caso, el salario del empleado asalariado es simplemente el salario base, por lo que el método calcularSalario() se sobrescribe para devolver el salario base.
+
+La clase EmpleadoPorHoras es otra subclase que hereda los atributos y métodos de la clase Empleado. En este caso, el salario del empleado por horas se calcula multiplicando las horas trabajadas por la tarifa por hora, y sumando ese resultado al salario base. El método calcularSalario() se sobrescribe para realizar este cálculo.
+
+La clase EmpleadoPorComision es una tercera subclase que hereda los atributos y métodos de la clase Empleado. En este caso, el salario del empleado por comisión se calcula multiplicando las ventas por el porcentaje de comisión, y sumando ese resultado al salario base. El método calcularSalario() se sobrescribe para realizar este cálculo.
+
+De esta manera, se pueden crear instancias de cada subclase y llamar al método calcularSalario() para obtener el salario correspondiente a cada tipo de empleado.*/
